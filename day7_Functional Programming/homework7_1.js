f1f89c89c22031bfc900ebd018c8bdae68bd4000
P-Amonrat <amonratpongka@gmail.com>
2022-04-27 21:27:33 +0700

    homeworkday7-Done

---
+++ day7_Functional Programming/homework7_1.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+let result1 = arr.filter (number => number%2 ===0);
+console.log(result1);
+
+let result2 = arr.map((number) => number*1000);
+console.log(result2);
+
