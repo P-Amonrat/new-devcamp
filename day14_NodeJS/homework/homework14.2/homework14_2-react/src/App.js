a63ef9e953dfcfe0187d6ece5a8b6ca26884dce7
P-Amonrat <amonratpongka@gmail.com>
2022-05-17 21:05:19 +0700

    Edit column and add header

--- day14_NodeJS/homework/homework14.2/homework14_2-react/src/App.js
+++ day14_NodeJS/homework/homework14.2/homework14_2-react/src/App.js
@@ -1,38 +1,3 @@
-// import { Table } from 'antd';
-// import axios from 'axios';
-// import { useEffect, useState } from 'react';
-
-// function App() {
-//   const columns = [
-//     {
-//       title: 'Avatar',
-//       dataIndex: 'avatar',
-//       key: 'avatar',
-//       render: (t, x) => <img src={x.avatar} />,
-//     },
-//     {
-//       title: 'First Name',
-//       dataIndex: 'first_name',
-//       key: 'First Name',
-//     },
-//     {
-//       title: 'Email',
-//       dataIndex: 'email',
-//       key: 'email',
-//     },
-//   ];
-//   const [data, setData] = useState([]);
-//   useEffect(() => {
-//     axios.get('/api/users').then((result) => {
-//       setData(result.data);
-//     });
-//   }, []);
-//   return <Table dataSource={data} columns={columns} pagination={false} />;
-// }
-
-// export default App;
-
-
 import axios from 'axios';
 import { Card, Col, Row } from 'antd';
 import { useEffect, useState } from 'react';
@@ -49,19 +14,22 @@ function App() {
   console.log(data)
 
   return (
-    data.map(x =>
-      <div className="site-card-wrapper">
-        <Row >
-          <Col span={4}>
-            <Card style={{justifyItems: "center"}}>
-              <p >{x.first_name}</p>
-              <p>{x.email}</p>
-              <img src={x.avatar} />
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    )
+    <>
+    <h1 style={{textAlign: "center", margin: "20px"}}>Hello ReqRes Users!</h1>
+      <Row >
+        {data.map(x => {
+          return (
+            <Col span={4}>
+              <Card>
+                <p >{x.first_name}</p>
+                <p>{x.email}</p>
+                <img src={x.avatar} />
+              </Card>
+            </Col >
+          )
+        })}
+      </Row>
+    </>
   )
 }
 
