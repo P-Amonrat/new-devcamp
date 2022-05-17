b413411cd6d9454954b7af3d53a6c5e237733d4d
P-Amonrat <amonratpongka@gmail.com>
2022-05-17 22:21:48 +0700

    Homework Day15-Finished

---
+++ day15_Database/homework15.2 & 15.3/Employee-2.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 13, 2022 at 08:30 AM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Employee`
+--
+
+CREATE TABLE `Employee` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Employee`
+--
+
+INSERT INTO `Employee` (`id`, `firstname`, `lastname`, `age`, `date`, `address`) VALUES
+(1, 'Neil', 'Armstrong', 34, '2022-05-13 06:47:34', NULL),
+(2, 'Leslie', 'Ellis', 24, '2022-05-13 06:47:34', NULL),
+(3, 'Sandra', 'Hoffman\r\n', 35, '2022-05-13 06:49:37', NULL),
+(4, 'Cameron', 'Meyer', 19, '2022-05-13 06:49:37', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Employee`
+--
+ALTER TABLE `Employee`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
